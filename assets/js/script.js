18ed1fe6dc3262b3d1e34105b81c33f5060837df
Mark <mjbc53@gmail.com>
2021-09-28 18:49:55 -0500

    second function created to create a card. have to add a for loop to it then its done

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,8 @@ var searchBtn = document.querySelector("#search-btn")
 //Current date forecast element call
 var currentDateForcast = document.querySelector("#current-date-forecast")
 
-//how to call data attributes
-var test = document.querySelector("[data-day='1']")
+var FFDCounter = 1
+
 
 //weather api key
 var apiKey = "5bdea9a85704cee38790f2d31b062496"
@@ -36,40 +36,43 @@ var fetchWeatherApi = function(search){
 
 //function to make current day display
 var currentForecastDisplay = function(){
-    var classes = ["col-12 border border-2 ps-2", "bottom-margin"]
+    var classes = {
+        CDFDiv: "col-12 border border-2 ps-2",
+        CDFOther: "bottom-margin",
+    }
     var CDFDiv = document.querySelector("#CDF-Div")
-    CDFDiv.classList = classes[0]
+    CDFDiv.classList = classes.CDFDiv
     currentDateForcast.appendChild(CDFDiv)
 
     var CDFTitle = document.createElement("h2")
-    CDFTitle.classList = classes[1]
+    CDFTitle.classList = classes.CDFOther
     CDFTitle.textContent = "atlanta"
     CDFDiv.appendChild(CDFTitle)
    
 
     var CDFTemp = document.createElement("p")
-    CDFTemp.classList = classes[1]
+    CDFTemp.classList = classes.CDFOther
     CDFTemp.innerHTML = "Temp: <span id='CDF-temp'></span>"
     CDFDiv.appendChild(CDFTemp)
    
     
 
     var CDFWind = document.createElement("p")
-    CDFWind.classList = classes[1]
+    CDFWind.classList = classes.CDFOther
     CDFWind.innerHTML = "Wind: <span id='CDF-wind'></span>"
     CDFDiv.appendChild(CDFWind)
    
     
 
     var CDFHumidity = document.createElement("p")
-    CDFHumidity.classList = classes[1]
+    CDFHumidity.classList = classes.CDFOther
     CDFHumidity.innerHTML = "Humidity: <span id='CDF-humidity'></span>"
     CDFDiv.appendChild(CDFHumidity)
    
     
 
     var CDFUVIndex = document.createElement("p")
-    CDFUVIndex.classList = classes[1]
+    CDFUVIndex.classList = classes.CDFOther
     CDFUVIndex.innerHTML = "UV Index: <span id='CDF-UV-index'></span>"
     CDFDiv.appendChild(CDFUVIndex)
    
@@ -78,7 +81,48 @@ var currentForecastDisplay = function(){
 
 
 var futureForecastDisplay = function(){
+    var card = document.querySelector("[data-day= '" + FFDCounter + "']")
+    var classes = {
+        cardTitle: "card-title",
+        cardText: "card-text",
+    }
 
+
+    var infoObj ={
+        temp: "temp:<span data-day-temp='1'></span>",
+        wind: "wind:<span data-day-wind='1'></span>",
+        humidity:"Humidity:<span data-day-humidity='1'></span>",
+    }
+    var tempDay = card
+    tempDay.classList = "card"
+
+    var h5 = document.createElement("h5")
+    h5.classList = classes.cardTitle
+    h5.textContent = "05/06/2020"
+    card.appendChild(h5)
+
+
+
+    var p1 = document.createElement('p')
+    p1.classList = classes.cardText
+    p1.innerHTML = infoObj.temp
+    card.appendChild(p1)
+    console.log(p1)
+
+
+    var p2 = document.createElement('p')
+    p2.classList = classes.cardText
+    p2.innerHTML = infoObj.wind
+    card.appendChild(p2)
+    console.log(p2)
+
+
+    var p3 = document.createElement('p')
+    p3.classList = classes.cardText
+    p3.innerHTML = infoObj.humidity
+    card.appendChild(p3)
+    console.log(p3)
+    
 }
 
 
@@ -89,4 +133,5 @@ searchBtn.addEventListener("click",function(){
     fetchWeatherApi(searchTerm)
 })
 
+futureForecastDisplay()
 currentForecastDisplay()
